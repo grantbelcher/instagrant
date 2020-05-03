335a3f0d67c36671a1c789d84d36ae60ba6059d0
Grant Belcher <gtbelch@gmail.com>
2020-05-03 15:44:21 -0700

    CORRECTLY applies AIRBNB styleguide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,28 @@
 module.exports = {
-  parser: 'babel-eslint',
   env: {
     browser: true,
-    commonjs: true,
     es6: true,
     node: true,
-    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 2020,
     ecmaFeatures: {
-      impliedStrict: true,
       jsx: true,
     },
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks'],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'airbnb'
+  plugins: [
+    'react',
   ],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
   rules: {
-    // You can do your customizations here...
-    // For example, if you don't want to use the prop-types package,
-    // you can turn off that recommended rule with: 'react/prop-types': ['off']
+    'no-console': 'off',
   },
-}
+};
