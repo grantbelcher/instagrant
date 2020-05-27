547ad8e21259b7642266e0aff60377de801122ef
Grant Belcher <gtbelch@gmail.com>
2020-05-27 16:37:04 -0700

    searches users when textinput value changes

--- client/src/components/NewChat.jsx
+++ client/src/components/NewChat.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -9,19 +10,26 @@ import DialogContent from '@material-ui/core/DialogContent';
 import IconButton from '@material-ui/core/IconButton';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-// const styles = {
-
-// };
 
 const NewChat = ({ open, setModalOpen }) => {
   const [query, setQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
 
+  const searchUsers = async () => {
+    const results = await axios.get(`http://localhost:1000/users?q=${query}`);
+    setSuggestions(results.data);
+  };
+
+  useEffect(() => {
+    searchUsers();
+  }, [query]);
+
+
   const handleClose = () => {
     setQuery('');
     setModalOpen(false);
   };
-  console.log(query);
+  console.log(suggestions);
   return (
     <Dialog
       open={open}
