f101484df429388fb369bb151f04fa5feaeb9c68
Grant Belcher <gtbelch@gmail.com>
2020-05-26 23:14:43 -0700

    makes text-fields in modal controlled components

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -7,34 +7,46 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-const Auth = ({ handleClose, open, form }) => (
-  <Dialog
-    open={open}
-    onClose={handleClose}
-    aria-labelledby="simple-modal-title"
-    aria-describedby="simple-modal-description"
-  >
-    <DialogTitle>{form}</DialogTitle>
-    <DialogContent>
-      <TextField
-        autoFocus
-        margin="dense"
-        id="name"
-        label="username"
-        fullWidth
-      />
-      <TextField
-        margin="dense"
-        id="name"
-        label="password"
-        fullWidth
-      />
-    </DialogContent>
-    <DialogActions>
-      <Button onClick={handleClose}>close</Button>
-    </DialogActions>
+const Auth = ({ handleClose, open, form }) => {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  console.log(name, password);
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    >
+      <DialogTitle>{form}</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="username"
+          fullWidth
+          required
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <TextField
+          margin="dense"
+          id="name"
+          label="password"
+          fullWidth
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>close</Button>
+      </DialogActions>
 
-  </Dialog>
-);
+    </Dialog>
+
+  );
+};
 
 export default Auth;
