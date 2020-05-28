6ae1ab864162e832fcb08ea92c970c423059432f
Grant Belcher <gtbelch@gmail.com>
2020-05-28 12:52:11 -0700

    connects to socket on login

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,8 +1,20 @@
 /* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
 import Dashboard from './Dashboard';
 
+const socketUrl = 'http://5546de74ed9d.ngrok.io/';
+
 const Main = () => {
+  const initSocket = () => {
+    io(socketUrl, () => {
+      console.log('connection made?');
+    });
+  };
+  useEffect(() => {
+    initSocket();
+  }, []);
+
   return (
     <>
       <Dashboard />
