70b51c25c5bb9414ea33e4e2288d65bec5caf206
Grant Belcher <gtbelch@gmail.com>
2020-06-07 16:21:40 -0700

    users chats render in chatlist

--- client/src/components/ChatList.jsx
+++ client/src/components/ChatList.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import moment from 'moment';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
-// import ChatListItem from './ChatListItem';
+import ChatListItem from './ChatListItem';
 
 
 const styles = {
@@ -15,32 +15,33 @@ const styles = {
 };
 
 // const ChatList = ({ activeChat, allChats, token }) => {
-const ChatList = () => {
+const ChatList = ({ allChats }) => {
+  console.log(allChats, 'allChats');
+  if (!allChats) return null;
   const [chatList, setChatList] = useState([]);
-  // useEffect(() => {
-  //   let chats = allChats.sort((a, b) => {
-  //     return moment(b.messages[b.messages.length - 1].date) - moment(a.messages[a.messages.length - 1].date);
-  //   });
-  //   chats = allChats.map((chat) => <ChatListItem button chat={chat} />);
-  //   setChatList(chats);
-  // }, [allChats]);
+  useEffect(() => {
+    let chats = allChats.sort((a, b) => {
+      return moment(b.messages[b.messages.length - 1].date) - moment(a.messages[a.messages.length - 1].date);
+    });
+    chats = allChats.map((chat) => <ChatListItem button chat={chat} />);
+    setChatList(chats);
+  }, [allChats]);
 
   return (
     <Paper style={styles.container}>
       <List>
-        hey
-        {/* {chatList} */}
+        {chatList}
       </List>
     </Paper>
   );
 };
 
-// const mapStateToProps = ({ chats, auth }) => {
-//   const { activeChat, chats: allChats } = chats;
-//   return {
-//     activeChat,
-//     allChats,
-//   };
-// };
+const mapStateToProps = ({ chat, auth }) => {
+  const { activeChat, chats: allChats } = chat;
+  return {
+    activeChat,
+    allChats,
+  };
+};
 
-export default connect(null, null)(ChatList);
+export default connect(mapStateToProps, null)(ChatList);
--- client/src/components/ChatListItem.jsx
+++ client/src/components/ChatListItem.jsx
@@ -1,111 +1,112 @@
-// /* eslint-disable no-underscore-dangle */
-// import React, { useEffect, useState } from 'react';
-// import { connect } from 'react-redux';
-// import moment from 'moment';
-// import PropTypes from 'prop-types';
-// import ListItem from '@material-ui/core/ListItem';
-// import ListItemText from '@material-ui/core/ListItemText';
-// import Divider from '@material-ui/core/Divider';
-// import UserIcon from './UserIcon';
-// import { selectChat } from '../redux/actions/chats';
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import UserIcon from './UserIcon';
+import { selectChat } from '../redux/actions/chats';
 
-// const styles = {
-//   activeStyle: {
-//     backgroundColor: '#f5f5f5',
-//   },
-//   boldFont: {
-//     fontWeight: 'bold',
-//   },
-// };
+const styles = {
+  activeStyle: {
+    backgroundColor: '#f5f5f5',
+  },
+  boldFont: {
+    fontWeight: 'bold',
+  },
+};
 
-// const ChatListItem = ({
-//   chat, currentUser, handleClick, activeChat,
-// }) => {
-//   const { users, messages, name } = chat;
-//   let recipients;
-//   let usernames;
-//   let recipient;
-//   let recipientAvatar;
-//   let lastMessage;
-//   let chatName;
-//   let lastActivity;
-//   let secondaryText;
-//   let primaryText;
-//   let date = '';
-//   if (messages & messages.length > 0) {
-//     date = moment(message[messages.length - 1].date).fromNow();
-//   }
+const ChatListItem = ({
+  chat, currentUser, activeChat,
+}) => {
+  const { users, messages, name } = chat;
+  let recipients;
+  let usernames;
+  let recipient;
+  let recipientAvatar;
+  let lastMessage;
+  let chatName;
+  let lastActivity;
+  let secondaryText;
+  let primaryText;
+  let date = '';
 
-//   let formattedDate = moment(date).fromNow();
-//   const [relativeTime, setRelativeTime] = useState(date);
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       formattedDate = moment(date).fromNow();
-//       setRelativeTime(formattedDate);
-//     }, 30000);
-//     return () => {
-//       clearInterval(interval);
-//     };
-//   }, []);
+  if (messages & messages.length > 0) {
+    date = moment(message[messages.length - 1].date).fromNow();
+  }
 
-
-//   if (users !== undefined && users.length > 0) {
-//     recipients = users.filter((user) => user._id !== currentUser._id);
-//     if (recipients.length > 1) {
-//       usernames = `${recipients[0].name}, ${recipients[1].name.substr(0, 4)}...`;
-//     } else {
-//       usernames = recipients[0].name;
-//     }
-//     recipient = recipients[0].name;
-//     recipientAvatar = recipients[0].avatar;
-//   }
-//   if (chat.name === 'Community') {
-//     chatName = 'Community Chat';
-//   }
-//   if (messages && messages.length > 0) {
-//     lastMessage = messages[messages.length - 1];
-//     lastActivity = moment(lastMessage.date).fromNow();
-//     primaryText = (
-//       <div style={activeChat._id === chat._id ? styles.boldFont : null}>{usernames || chatName}</div>
-//     );
-//     secondaryText = (
-//       <>
-//         <div style={activeChat._id === chat._id ? styles.boldFont : null}>{`${lastMessage.username}: ${lastMessage.text.substr(0, 15)}...`}</div>
-//         <div style={activeChat._id === chat._id ? styles.boldFont : null}>{lastActivity}</div>
-//       </>
-//     );
-//   }
-//   return (
-//     <>
-//       <ListItem
-//         style={activeChat._id === chat._id ? styles.activeStyle : null}
-//         button
-//         onClick={() => handleClick(chat)}
-//       >
-//         <UserIcon name={recipient} imgUrl={recipientAvatar} />
-//         <ListItemText
-//           primary={primaryText}
-//           secondary={secondaryText}
-//           // secondary={lastMessage.username ? `${lastMessage.username}: ${lastMessage.text.substr(0, 13)}...\n${lastActivity}` : 'loading'}
-//         />
-//       </ListItem>
-//       <Divider />
-//     </>
-//   );
-// };
+  let formattedDate = moment(date).fromNow();
+  const [relativeTime, setRelativeTime] = useState(date);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      formattedDate = moment(date).fromNow();
+      setRelativeTime(formattedDate);
+    }, 30000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
 
-// const mapStateToProps = ({ auth, chats }) => {
-//   const { user: currentUser } = auth;
-//   const { activeChat } = chats;
-//   return {
-//     currentUser,
-//     activeChat,
-//   };
-// };
+  if (users !== undefined && users.length > 0) {
+    recipients = users.filter((user) => user._id !== currentUser._id);
+    if (recipients.length > 1) {
+      usernames = `${recipients[0].name}, ${recipients[1].name.substr(0, 4)}...`;
+    } else {
+      usernames = recipients[0].name;
+    }
+    recipient = recipients[0].name;
+    recipientAvatar = recipients[0].avatar;
+  }
+  if (chat.name === 'Community') {
+    chatName = 'Community Chat';
+  }
+  if (messages && messages.length > 0) {
+    lastMessage = messages[messages.length - 1];
+    lastActivity = moment(lastMessage.date).fromNow();
+    primaryText = (
+      <div style={activeChat._id === chat._id ? styles.boldFont : null}>{usernames || chatName}</div>
+    );
+    secondaryText = (
+      <>
+        <div style={activeChat._id === chat._id ? styles.boldFont : null}>{`${lastMessage.username}: ${lastMessage.text.substr(0, 15)}...`}</div>
+        <div style={activeChat._id === chat._id ? styles.boldFont : null}>{lastActivity}</div>
+      </>
+    );
+  }
+  return (
+    <>
+      <ListItem
+        style={activeChat._id === chat._id ? styles.activeStyle : null}
+        button
+        onClick={() => handleClick(chat)}
+      >
+        <UserIcon name={recipient} imgUrl={recipientAvatar} />
+        <ListItemText
+          primary={primaryText}
+          secondary={secondaryText}
+          // secondary={lastMessage.username ? `${lastMessage.username}: ${lastMessage.text.substr(0, 13)}...\n${lastActivity}` : 'loading'}
+        />
+      </ListItem>
+      <Divider />
+    </>
+  );
+};
+
+
+const mapStateToProps = ({ auth, chat }) => {
+  const { user: currentUser } = auth;
+  const { activeChat } = chat;
+  return {
+    currentUser,
+    activeChat,
+  };
+};
 
 // const mapDispatchToProps = {
 //   handleClick: selectChat,
 // };
 
-// export default connect(mapStateToProps, mapDispatchToProps)(ChatListItem);
+export default connect(mapStateToProps, null)(ChatListItem);
