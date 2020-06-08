c90c871bc0f69422215d08c80e0e1e32aab85b76
Grant Belcher <gtbelch@gmail.com>
2020-06-08 11:05:01 -0700

    changes badge styling in user icon

--- client/src/components/UserIcon.jsx
+++ client/src/components/UserIcon.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import Badge from '@material-ui/core/Badge';
 import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: '$ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}))(Badge);
+
 
 const UserIcon = ({ name, imgUrl, connectedUsers }) => {
 // const UserIcon = ({ name, imgUrl }) => {
@@ -11,7 +41,7 @@ const UserIcon = ({ name, imgUrl, connectedUsers }) => {
   const connected = (connectedUsers[name] !== undefined);
   return (
     <ListItemAvatar>
-      <Badge
+      <StyledBadge
         color="secondary"
         badgeContent="10"
         variant="dot"
@@ -19,7 +49,7 @@ const UserIcon = ({ name, imgUrl, connectedUsers }) => {
         overlap="circle"
       >
         <Avatar alt={name} src={imgUrl} />
-      </Badge>
+      </StyledBadge>
     </ListItemAvatar>
   );
 };
