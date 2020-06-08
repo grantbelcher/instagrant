c5c765e16043b8fe92eb206a1fb96cca4a4f3e78
Grant Belcher <gtbelch@gmail.com>
2020-06-08 11:33:44 -0700

    properly formats usernames in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,8 +23,12 @@ const Header = ({ activeChat, user }) => {
   userNames = users.reduce((acc, obj) => `${acc + obj.name}, `, '');
   allNames = userNames.substr(0, userNames.length - 2);
   userNames = userNames.substr(0, userNames.length - 2);
-  if (userNames.length > 60) {
-    userNames = `${userNames.substr(0, 60)}...`;
+  if (userNames.length > 47) {
+    userNames = `${userNames.substr(0, 47)}...`;
+    if (userNames[46] === ' ') {
+      userNames = `${userNames.slice(0, 46)}...`;
+      console.log(userNames, 'after');
+    }
   }
 
 
@@ -41,7 +45,7 @@ const Header = ({ activeChat, user }) => {
         ))}
       </AvatarGroup>
       <Tooltip title={allNames}>
-        <span>
+        <span style={{ fontSize: 'larger' }}>
           {userNames}
         </span>
       </Tooltip>
